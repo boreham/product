cb4a6d7c362dcca0ce5dac39c6cfcc45f5e66ae5
Kong Boreham <99660515+boreham@users.noreply.github.com>
2025-01-15 16:35:00 +0300

    Update Program.cs

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,7 +2,6 @@ using Client.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient<ProductService>(client =>
 {
